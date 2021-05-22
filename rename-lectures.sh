8f0ee7927710f55bdd9216a4bd7ccfe573a8bcfc
Josiah Bull <josiah.bull7@gmail.com>
2021-05-22 22:12:12 +1200

    feat (rename-lectures)
    A small bash script for renaming lecture files to something more readable during bulk watching.

---
+++ rename-lectures.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Josiah Bull - 22/05/2021
+#A small script to rename lecture files to a more readable format.
+
+for entry in "$PWD"/*.{mp4,m4v}
+do
+ f="${entry##*/}"
+ if [ ${f:4:1} != "-" ]
+ then
+  echo "File renamed: $f"
+  mv "$entry" ${f:0:4}-${f:4:2}-${f:6:2}-${f:8}
+ else
+  echo "File already renamed: $f"
+ fi
+done
